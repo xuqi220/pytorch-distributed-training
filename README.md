dc0fc96b941226ae2307cdaf9f0ed6cb72ab9fdb
welch <welch_cn@163.com>
2024-05-12 00:08:06 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 分布式训练
-该仓库记录基于pytorch的分布式训练学习笔记。
+该仓库记录基于pytorch提供的DDP以及DeepSpeed的分布式训练的学习笔记
 
 ## 环境
 
@@ -9,7 +9,7 @@ torch==2.1.0
 本实验的数据是随机生成的4096条数据、模型只有一层dense层。在单机多卡（4 * A100-80G GPU）上进行的，当然也支持内存更小的多卡机器。本实验的代码结构简单，提供了注释，可读性强。
 
 ## 开始
-1.利pytorch提供的torch.multiprocessing 包启动多进程执行训练脚本
+1.利用pytorch提供的torch.multiprocessing 包启动多进程执行训练脚本
 ```
 python mp.py --max_epochs=2 --batch_size=32
 
@@ -28,5 +28,38 @@ GPU:1 | Epoch: 1 | Batchsize: 32 | Steps: 32
 
 Batchsize\*step\*GPU_NUM = 32\*32\*4 = 4096
 
+2.利用pytorch提供的torchrun 或 torch.distributed.launch 启动多进程执行训练脚本
+```
+torchrun --nproc-per-node=4 start_with_torchrun.py --max_epoch=2 --batch_size=32
+
+GPU_Count: 4
+GPU:2 | Epoch: 0 | Batchsize: 32 | Steps: 32
+GPU:3 | Epoch: 0 | Batchsize: 32 | Steps: 32
+GPU:1 | Epoch: 0 | Batchsize: 32 | Steps: 32
+GPU:0 | Epoch: 0 | Batchsize: 32 | Steps: 32
+GPU:3 | Epoch: 1 | Batchsize: 32 | Steps: 32
+GPU:2 | Epoch: 1 | Batchsize: 32 | Steps: 32
+GPU:1 | Epoch: 1 | Batchsize: 32 | Steps: 32
+GPU:0 | Epoch: 1 | Batchsize: 32 | Steps: 32
+
+*****************************************
+
+python -m torch.distributed.launch --use-env --nproc-per-node=4 start_with_torchrun.py --max_epoch=2 --batch_size=32
+
+GPU_Count: 4
+GPU:0 | Epoch: 0 | Batchsize: 32 | Steps: 32
+GPU:2 | Epoch: 0 | Batchsize: 32 | Steps: 32
+GPU:1 | Epoch: 0 | Batchsize: 32 | Steps: 32
+GPU:3 | Epoch: 0 | Batchsize: 32 | Steps: 32
+GPU:0 | Epoch: 1 | Batchsize: 32 | Steps: 32
+GPU:2 | Epoch: 1 | Batchsize: 32 | Steps: 32
+GPU:1 | Epoch: 1 | Batchsize: 32 | Steps: 32
+GPU:3 | Epoch: 1 | Batchsize: 32 | Steps: 32
+```
+
+
+torchrun 命令 等价于 python -m torch.distributed.launch --use-env
+
+torchrun 将'LOCAL_RANK'设置环境变量中，用户需要从`os.environ('LOCAL_RANK')`中取。
 
 
---
+++ start_with_torchrun.py
@@ -0,0 +1,93 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+# 引入分布式包
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+import torch.multiprocessing as mp
+
+# ddp setup
+def ddp_setup():
+    # 初始化进程组
+    init_process_group(backend='nccl')
+    torch.cuda.set_device(int(os.environ['LOCAL_RANK']))
+
+# 随机生成数据集
+class MyDataset(Dataset):
+    def __init__(self, size) -> None:
+        super().__init__()
+        self.size = size
+        self.data = [(torch.rand(20), torch.rand(1)) for _ in range(size)]
+    
+    def __len__(self):
+        return self.size
+    
+    def __getitem__(self, index):
+        return self.data[index]
+
+class Trainer():
+    def __init__(self,
+                 model:nn.Module,
+                 train_dataloader: DataLoader,
+                 optimizer: torch.optim.Optimizer):
+        # torchrun会自动将当前进程号放到os环境变量中，用户直接获取即可
+        self.gpu_id = int(os.environ['LOCAL_RANK'])
+        self.model = model.to(self.gpu_id)
+        self.train_dataloader = train_dataloader
+        self.optimizer = optimizer
+        # 用ddp包装模型
+        self.model = DDP(model, device_ids=[self.gpu_id])
+
+    def train(self, max_epoch:int):
+        for epoch in range(max_epoch):
+            self._run_epoch(epoch)
+    
+    def _run_epoch(self, epoch):
+        batch_size = len(next(iter(self.train_dataloader))[0])
+        self.train_dataloader.sampler.set_epoch(epoch)
+        step=0
+        for x, y in self.train_dataloader:
+            x = x.to(self.gpu_id)
+            y = y.to(self.gpu_id)
+            self._run_batch(x, y)
+            step+=1
+        print(f"GPU:{self.gpu_id} | Epoch: {epoch} | Batchsize: {batch_size} | Steps: {step}")
+        
+
+    def _run_batch(self, x, y):
+        self.optimizer.zero_grad()
+        y_ = self.model(x)
+        loss = F.cross_entropy(y_, y)
+        loss.backward()
+        self.optimizer.step()
+
+def main(max_epochs:int, batch_size:int):
+    ddp_setup()
+    if int(os.environ['LOCAL_RANK'])==0:# 在0号进程上打印信息
+        print(f"GPU_Count: {torch.cuda.device_count()}")
+    train_dataset = MyDataset(4096)
+    # 分布式训练需要将数据划分（无重复）到不同的设备上，DistributedSampler自动帮我们完成
+    td = DistributedSampler(train_dataset)
+    train_dataloader = DataLoader(train_dataset, 
+                                  batch_size=batch_size,
+                                  sampler=td)
+    model = nn.Linear(20, 1)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+    trainer = Trainer(model, train_dataloader, optimizer)
+    trainer.train(max_epochs)
+    # 销毁进程组
+    destroy_process_group()
+
+# 分布式训练
+if __name__=="__main__":
+    import argparse # 从命令行接收参数
+    parser = argparse.ArgumentParser(description="distributed training toy case")
+    parser.add_argument("--max_epochs", type=int, default=5)
+    parser.add_argument("--batch_size", type=int, default=32)
+    args = parser.parse_args()
+
+    main(args.max_epochs, args.batch_size)
+
